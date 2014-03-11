1a207491d3fe17a6c15e720de7c1ed9da0968d02
Ivanis KOUAME <ivanis.kouame@gmail.com>
2014-03-11 19:17:15 +0100

    Remote interface

---
+++ Source/src/nl/dcs/rmi/RMIInterface.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package nl.dcs.rmi;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import nl.dcs.da.tss.events.Event;
+
+/**
+ *
+ * @author Ivanis
+ */
+public interface RMIInterface extends Remote {
+    
+    public void recieveEvent (long sender, Event e) throws RemoteException;
+    
+    
+}
