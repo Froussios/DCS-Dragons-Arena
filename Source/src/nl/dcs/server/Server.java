1a10a69ba5d623cf701756e948515f94039e0907
Ivanis KOUAME <ivanis.kouame@gmail.com>
2014-03-12 01:45:01 +0100

    Continuing the server

--- Source/src/nl/dcs/server/Server.java
+++ Source/src/nl/dcs/server/Server.java
@@ -7,6 +7,9 @@
 package nl.dcs.server;
 
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
 import nl.dcs.da.tss.events.Event;
 
 
@@ -16,8 +19,14 @@ import nl.dcs.da.tss.events.Event;
  */
 public class Server {
     
-    public void main (String[] args){
-        
+    public static void main (String[] args) throws RemoteException{
+        try {
+        	
+            RMIInterface skeleton = (RMIImplementation) UnicastRemoteObject.exportObject(new RMIImplementation(), 10000); 
+            Registry registry = LocateRegistry.createRegistry(10000);
+            registry.rebind("Recieve", skeleton); // publie notre instance sous le nom "Add"
+        } catch (RemoteException e) {
+        }
     }
 
 }
